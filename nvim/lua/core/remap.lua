2203a0d4aada77ce432bfd1eafae9217ccca7705
SaHHiiLL <sahilpanchal2903@gmail.com>
2023-01-06 22:13:13 +0000

    lsp improvements

--- nvim/lua/core/remap.lua
+++ nvim/lua/core/remap.lua
@@ -7,10 +7,13 @@ vim.keymap.set("n", "<leader>o", vim.cmd.NvimTreeFocus)
 vim.keymap.set("n", "<leader>tff", ":Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>tss", ":Telescope spell_suggest<CR>")
 
-
+-- move reset --
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 
 -- Toggle term --
 vim.keymap.set("n", "<leader>T", ":ToggleTerm direction=float size=50<CR>")
+
+-- comment shortcut -- 
+vim.keymap.set("v", "<C-]>", "gc")

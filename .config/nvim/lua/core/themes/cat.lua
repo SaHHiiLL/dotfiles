b1033b957ba202c2023b453e50c098d4a71289cf
Sahil <sahilpanchal2903@gmail.com>
2024-04-08 23:44:38 +0100

    Changed Colours

--- .config/nvim/lua/core/themes/cat.lua
+++ .config/nvim/lua/core/themes/cat.lua
@@ -1,9 +1,14 @@
 local M = {
     flavour = "mocha",
-    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-    no_bold = true, -- Force no bold
+    term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    no_bold = false, -- Force no bold
     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
         comments = { "italic" },
+    },
+    color_overrides = {
+        mocha = {
+            base = '#11111b'
+        }
     }
 }
 

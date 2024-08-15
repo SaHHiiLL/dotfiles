0c4cf900c0b6005a1972b6e287e13d353aed7400
Sahil <sahilpanchal2903@gmail.com>
2024-08-15 14:57:15 +0100

    Updated

--- .config/nvim/lua/core/plugin_config/telescope.lua
+++ .config/nvim/lua/core/plugin_config/telescope.lua
@@ -4,16 +4,16 @@ local u = require("core.utils")
 
 u.Nmap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", "File Finder")
 u.Nmap("<leader>fw", "<cmd>Telescope grep_string<CR>", "Live Grep")
+u.Nmap("<leader>fs", "<cmd>Telescope lsp_document_symbols<CR>", "Gets workspace symbols")
 
 u.Nmap("<leader>gc", "<cmd>Telescope git_commits<CR>", "Git Commits")
-u.Nmap("<leader>lws", "<cmd>Telescope lsp_workspace_symbols", "Gets workspace symbols")
 
 local M = {}
 
 M = {
 pickers = {
     colorscheme = {
-      enable_preview = true
+      enable_preview = false
     }
   }
 }

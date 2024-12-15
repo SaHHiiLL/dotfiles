861b9461eee7779d86aeea6f3775c9c87a6400f4
Sahil <sahilpanchal2903@gmail.com>
2024-12-15 08:39:47 +0000

    removed old python script for fastercd AND change aliases

--- .config/scripts/faster_cd/fzfcd.sh
+++ .config/scripts/faster_cd/fzfcd.sh
@@ -1,6 +1,28 @@
+###
+### Added:  December 15, 2024
+### By:     SaHHiiLL
+###
 #!/usr/bin/env bash
-#
-path=$($HOME/.config/scripts/faster_cd/listfiles.py | fzf)
+
+programing_dir() {
+    find ~/programing/ -maxdepth 2 -type d | tail -n +2
+}
+
+home_dir() {
+    find ~ -maxdepth 1 -type d -not -name '.*' | tail -n +2
+}
+
+dotfiles() {
+    find ~/.config/ -maxdepth 1 -type d
+}
+
+path=$(
+    {
+        echo $(programing_dir);
+        echo $(home_dir);
+        echo $(dotfiles);
+    } | tr ' ' '\n' | fzf
+)
 
 if [[ $path ]]; then
     cd $path

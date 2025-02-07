fc794162a19b2cb9c7b703aef0461a344aa2c1ba
Sahil <sahilpanchal2903@gmail.com>
2025-02-07 17:57:27 +0000

    idk

---
+++ fabric/cava.py
@@ -0,0 +1,75 @@
+import os
+import struct
+import subprocess
+import tempfile
+
+from multiprocessing import Process as QueueProcess, Queue
+
+class Cava:
+    def __init__(self, bar_number=10, bit_format = "16bit", raw_target="/dev/stdout"):
+        self.source = None
+        self.process = None
+        self.config_temp = """
+        [general]
+        bars = %d
+        [output]
+        method = raw
+        raw_target = %s
+        bit_format = %s
+        """
+        self.bit_format = bit_format
+        self.bar_number = bar_number
+        self.raw_target = raw_target
+        self.config = self.config_temp % (self.bar_number, self.raw_target, self.bit_format)
+        self.bytetype, self.bytesize, self.byte_norm = ("H", 2, 65535) if self.bit_format == "16bit" else ("B", 1, 255)
+
+        self.chunk = self.bytesize * self.bar_number
+        self.fmt = self.bytetype * self.bar_number
+
+    def set_source(self):
+        if self.raw_target != "/dev/stdout":
+            if not os.path.exists(self.raw_target):
+                os.mkfifo(self.raw_target)
+            self.source = open(self.raw_target, "rb")
+        else:
+            print("Setting source")
+            self.source = self.process.stdout
+
+    def run(self, output_queue_buffer):
+        with tempfile.NamedTemporaryFile() as config_file:
+            config_file.write(self.config.encode())
+            config_file.flush()
+
+            self.process = subprocess.Popen(["cava", "-p", config_file.name], stdout=subprocess.PIPE)
+            if self.raw_target != "/dev/stdout":
+                if not os.path.exists(self.raw_target):
+                    os.mkfifo(self.raw_target)
+                source = open(self.raw_target, "rb")
+            else:
+                source = self.process.stdout
+            while True:
+                data = source.read(self.chunk)
+                if len(data) < self.chunk:
+                    break
+                sample = [i / self.byte_norm for i in struct.unpack(self.fmt, data)]
+                output_queue_buffer.put(sample)
+
+
+if __name__ == "__main__":
+    import time
+    def poll(input_queue):
+        while True:
+            data = input_queue.get()
+            if data is None:
+                continue
+            print("Polling ", data)
+
+    cava = Cava()
+    queue = Queue()
+    runner = QueueProcess(target=cava.run, args=(queue,))
+    # poller = QueueProcess(target=poll, args=(queue,))
+
+    runner.start()
+    # poller.start()
+
+    poll(queue)
--- fabric/main.py
+++ fabric/main.py
@@ -1,31 +1,105 @@
-import fabric
 from fabric import Application
 from fabric.hyprland.widgets import Workspaces
-from fabric.widgets.datetime import DateTime
 from fabric.widgets.box import Box
+from fabric.widgets.datetime import DateTime
 from fabric.widgets.label import Label
 from fabric.widgets.centerbox import CenterBox
+from fabric.widgets.scale import Scale
 from fabric.widgets.wayland import WaylandWindow as Window
+from fabric.widgets.eventbox import EventBox
+
+from multiprocessing import Process as QueueProcess, Queue
+
+from cava import Cava
+
+class VolumeControlAndCava:
+
+    def poll(self):
+        """
+        Polls the queue for data and updates the bars
+        """
+        while True:
+            self.poll_once(self._queue)
+
+    def poll_once(self, queue):
+        """
+        Polls the queue for data and updates the bars
+        at a rate of 1 second
+        :param queue:
+        """
+        data = queue.get()
+        if data is not None:
+            self._bars = data
+
+    def __init__(self, cava_rps=1, **kwargs):
+        self._cava_rps = cava_rps
+        self._last_time = 0
+        self._bars = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self._bars_string_rep = ["▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" ]
+        self._cava = Cava(bar_number=14)
+        self._queue = Queue()
+        self._cava_queue_thread = QueueProcess(target=self._cava.run, args=(self._queue,))
+        self._cava_poll_thread = QueueProcess(target=self.poll)
+
+    def start(self):
+        """
+        Start the cava process
+        """
+        self._cava_queue_thread.start()
+        self._cava_poll_thread.start()
+
+    def get_bar_from_volume(self, height):
+        """
+        Get a bar from `self._bars_string_rep` based on height
+        :param height: must be between 0 and 1
+        :return:
+        """
+        print("Height ", height)
+        if height == 0:
+            return ""
+
+        return self._bars_string_rep[int(height * 7) - 1]
+
+    def get_cava_bars(self)-> [str]:
+        """
+        Get the bars from the volume
+        :return: [str]
+        """
+        return [self.get_bar_from_volume(h) for h in self._bars]
+
+    def get_cava_label(self) -> Label :
+        """
+        Get the bars from the volume
+        :return: [str]
+        """
+        return Label("-".join(self.get_cava_bars()))
+
 
 class Bar(Window):
     def __init__(self, **kwargs):
         super().__init__(
-            layer="top",
+            style_classes=["bar-class"],
             anchor="left top right",
-            exclusivity="auto",
+            exclusivity='auto',
+            v_align="fill",
+            inspector=True,
             **kwargs
         )
-
+        self.volume_cava = VolumeControlAndCava()
+        self.volume = 0
         self.left_children = [DateTime()]
-        self.right_children = [Label("Hello world")]
-        self.center_children = [Workspaces()]
+        self.right_children = [Box(child=self.volume_cava.get_cava_label())]
+        self.volume_cava.start()
+        self.center_children = [Workspaces(style_classes=["workspaces"])]
 
         self.children = CenterBox(start_children=self.left_children,
                                   center_children=self.center_children,
-                                  end_children=self.right_children)
+                                  end_children=self.right_children,
+                                  orientation='h',
+                                  style='padding: 10px;')
 
 if __name__ == "__main__":
     bar = Bar()
-    app = Application("Bar", bar)
-    app.set_stylesheet_from_file("/home/Sahil/dotfile/fabric/styles/build/bar.css")
+    app = Application("bar-example", bar)
+    app.set_stylesheet_from_file("/home/Sahil/dotfile/fabric/styles/bar.css")
     app.run()

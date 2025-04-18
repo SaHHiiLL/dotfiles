dfe5e5b1383113ef4496d979b9a3e04607dc50d7
Sahil <sahilpanchal2903@gmail.com>
2025-04-18 12:06:23 +0000

    idk

--- .config/nvim/lua/core/lsp/treesitter.lua
+++ .config/nvim/lua/core/lsp/treesitter.lua
@@ -1,4 +1,18 @@
 print("Loading treesitter.lua")
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.pest = {
+  install_info = {
+    url = "https://github.com/pest-parser/tree-sitter-pest", -- local path or git repo
+    files = { "src/parser.c" },                              -- note that some parsers also require src/scanner.c or src/scanner.cc
+    -- optional entries:
+    branch = "main",                                         -- default branch in case of git repo if different from master
+    generate_requires_npm = false,                           -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false,                  -- if folder contains pre-generated src/parser.c
+  },
+  filetype = "pest",                                         -- if filetype does not match the parser name
+}
+
 local M = {
   highlight = {
     enable = true,
@@ -23,7 +37,6 @@ local M = {
     "zig",
     "python",
     "yuck",
-    "hyprlang",
   },
 }
 return M
